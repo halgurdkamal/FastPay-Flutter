4082e385e3a7772f5fcb3e84fac6f0b15bb5eaff
mac <mac@MacBook-Pro.local>
2023-02-14 18:55:06 +0300

    fix ios bug

--- ios/Classes/FastpayPlugin.m
+++ ios/Classes/FastpayPlugin.m
@@ -1,11 +1,11 @@
 #import "FastpayPlugin.h"
-#if __has_include(<fastpay/fastpay-Swift.h>)
-#import <fastpay/fastpay-Swift.h>
+#if __has_include(<fastpay/fastpay_iraq-Swift.h>)
+#import <fastpay/fastpay_iraq-Swift.h>
 #else
 // Support project import fallback if the generated compatibility header
 // is not copied when this plugin is created as a library.
 // https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
-#import "fastpay-Swift.h"
+#import "fastpay_iraq-Swift.h"
 #endif
 
 @implementation FastpayPlugin
